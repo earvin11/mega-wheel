b727888afd0468d37be4a0fe8a65affea923a338
Earvin Gomez <earvinsantiago.11@gmail.com>
2023-11-07 16:42:12 -0400

    round by redis

--- app/Bet/infraestructure/bet.controller.ts
+++ app/Bet/infraestructure/bet.controller.ts
@@ -1,27 +1,36 @@
 import { HttpContext } from '@adonisjs/core/build/standalone'
+import Redis from '@ioc:Adonis/Addons/Redis'
 import { BetUseCases } from '../application/bet.use-cases'
 import { BetEntity } from '../domain'
 import { getBetEarnings, useWinnerFilter } from 'App/Shared/Helpers/wheel-utils'
 import { RoundUseCases } from 'App/Round/application/round.use-cases'
 import { WheelFortuneUseCases } from 'App/WheelFortune/apllication/wheel-fortune.use-cases'
+import { RoundControlRedisUseCases } from '../../Round/application/round-control.redis.use-cases';
 
 export class BetController {
   constructor(
     private betUseCases: BetUseCases,
     private roundUseCases: RoundUseCases,
-    private wheelFortuneUseCases: WheelFortuneUseCases
+    private wheelFortuneUseCases: WheelFortuneUseCases,
+    private roundControlRedisUseCases: RoundControlRedisUseCases
   ) {}
 
   public createBet = async (ctx: HttpContext) => {
     const { request, response } = ctx
 
+    const providerId = request.body().porivderId
     const bet = { ...request.body() }
 
     try {
-      const round = await this.roundUseCases.findRoundByUuid(bet.round)
+      // const round = await this.roundUseCases.findRoundByUuid(bet.round)
+      
+      const round = await Redis.get(`round:${ bet.round }`)
       if (!round)
         return response.status(404).json({ error: 'No se encuentra el round' })
-      
+
+      const phaseRound = await this.roundControlRedisUseCases.getPhase(providerId)
+      if(phaseRound !== 'bet_time') return response.unauthorized({ error: 'Round closed' });
+
       const createBet = await this.betUseCases.createBet(bet as BetEntity)
 
       return response.status(201).json({ message: 'Bet creado!', createBet })
