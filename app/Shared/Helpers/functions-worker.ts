69b4db1a37b855bbbd83857e34570a2766423b0a
Earvin Gomez <earvinsantiago.11@gmail.com>
2023-12-01 09:36:01 -0400

    currencyUseCases in roundController

--- app/Shared/Helpers/functions-worker.ts
+++ app/Shared/Helpers/functions-worker.ts
@@ -5,7 +5,6 @@ import RoundModel from '../../Round/infraestructure/round.model'
 import BetModel from '../../Bet/infraestructure/bet.model'
 import PlayerModel from '../../Player/infraestructure/player.model'
 // import CurrencyModel from "../../Currencies/infrastructure/currency.model";
-import { WheelFortuneEntity } from 'App/WheelFortune/domain/wheel-fortune.entity'
 import GameModel from '../../WheelFortune/infraestructure/wheelFortune.model'
 import { logUseCases } from '../../Log/infrastructure/dependencies'
 import { TypesLogsErrors } from '../../Log/domain/log.entity'
